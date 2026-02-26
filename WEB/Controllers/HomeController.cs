ec16143d2ede53949d48f305d61a4074084033a0
Adam Svatoš <svatosa24@spstrutnov.cz>
2026-02-26 16:45:27 +0100

    Další stránky

--- WEB/Controllers/HomeController.cs
+++ WEB/Controllers/HomeController.cs
@@ -1,42 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
-using WEB.Models;
 using System.Diagnostics;
+using WEB.Models;
 
 namespace WEB.Controllers
 {
     public class HomeController : Controller
     {
-        // Hlavní stránka
         public IActionResult Index()
         {
             return View();
         }
 
-        // Tréninkový plán
         public IActionResult Plan()
         {
             return View();
         }
 
-        // Reference
         public IActionResult Reference()
         {
             return View();
         }
 
-        // Kontakt
         public IActionResult Kontakt()
         {
             return View();
         }
 
-        // Privacy (původní stránka)
         public IActionResult Privacy()
         {
             return View();
         }
 
-        // Error stránka
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
