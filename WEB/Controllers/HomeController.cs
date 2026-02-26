3fbee6f5f81ab84419999988e687d0afbeb25358
Adam Svatoš <svatosa24@spstrutnov.cz>
2026-02-26 16:40:26 +0100

    Přidání další stránky

--- WEB/Controllers/HomeController.cs
+++ WEB/Controllers/HomeController.cs
@@ -1,25 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using WEB.Models;
+using System.Diagnostics;
 
 namespace WEB.Controllers
 {
     public class HomeController : Controller
     {
+        // Hlavní stránka
         public IActionResult Index()
         {
             return View();
         }
 
+        // Tréninkový plán
+        public IActionResult Plan()
+        {
+            return View();
+        }
+
+        // Reference
+        public IActionResult Reference()
+        {
+            return View();
+        }
+
+        // Kontakt
+        public IActionResult Kontakt()
+        {
+            return View();
+        }
+
+        // Privacy (původní stránka)
         public IActionResult Privacy()
         {
             return View();
         }
 
+        // Error stránka
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
